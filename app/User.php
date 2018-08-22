23b37cad09b1445a314018daac6294bd3575c5f5
Harold <h.l.armada@outlook.com>
2018-08-22 16:21:23 +0800

    install laravel collective

--- app/User.php
+++ app/User.php
@@ -26,4 +26,25 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
+    public function student(){
+        return $this->belongsTo('App\Students');
+    }
+    public function course(){
+        return $this->belongsTo('App\Course');
+    }
+
+    public function isAdmin(){
+        if($this->role != null && $this->role->role == 'admin'  ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
